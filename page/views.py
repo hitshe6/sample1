419d447d20d9975dda17b26e10c1a11ccf7988f1
hitshe6 <hiteshprajapati4377@gmail.com>
2020-06-02 19:44:55 +0530

    first commit

---
+++ page/views.py
@@ -0,0 +1,16 @@
+
+from django.shortcuts import render
+
+from . models import Page
+
+def index(request, pagename):
+    pagename = '/' + pagename
+    pg = Page.objects.get(permalink=pagename)
+    context = {
+        'title': pg.title,
+        'content': pg.bodytext,
+        'last_updated': pg.update_date,
+        'page_list': Page.objects.all(),
+    }
+ 
+    return render(request, 'page/page.html', context)
